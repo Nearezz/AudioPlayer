c3b891932852d0c341bc0170dae616c0a4a8d044
Nearezz <biswasindranil019>
2024-03-13 09:27:10 -0400

    Initial Commit

---
+++ AudioPlayer/AudioPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioPlayerApp.swift
+//  AudioPlayer
+//
+//  Created by NYCDOE on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct AudioPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
