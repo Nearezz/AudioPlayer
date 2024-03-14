05c5a0edb8260f1075c03ddb72462f257a2347f0
Nearezz <biswasindranil019@gmail.com>
2024-03-13 23:26:32 -0400

    setup complete daddy

--- AudioPlayer/ContentView.swift
+++ AudioPlayer/ContentView.swift
@@ -16,16 +16,36 @@ struct ContentView: View {
     @State private var progress: Double = 0.0
     @State private var timer: Timer?
     
-    
-    
     var body: some View {
         VStack {
-           
-            
-        
+            HStack {
+                Button(action: {self.play()}) {
+                    Image(systemName: "play.sqaure.fill")
+                        .font(.system(size: 50.0))
+                }
+                Button(action: {self.pause()}) {
+                    Image(systemName: "pause.rectangle.fill")
+                        .font(.system(size: 50.0))
+                }
+                Button(action: {self.stop()}) {
+                    Image(systemName: "xmark.sqaure.fill")
+                        .font(.system(size: 50.0))
+                }
+                ProgressView("Progress",value: progress,total: 1.0)
+                    .id(progress)
+                    .frame(width:200,height:200)
+            }
         }
         
+        
+        
     }
+    
+    
+    func play() {}
+    func pause() {}
+    func stop() {}
+    
 }
 
 #Preview {
